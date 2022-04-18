6c63d62bcdf80d799b88102333269eace4eb575d
dhohirpradana <dhohirkudus@gmail.com>
2022-04-19 04:47:25 +0700

    update

--- models/product.js
+++ models/product.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       Product.belongsToMany(models.Category, {
         as: "categories",
         through: {
-          model: "productCategory",
+          model: "ProductCategories",
           as: "bridge",
         },
         foreignKey: "idProduct",
